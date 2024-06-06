1f1d6320fe22c4007462a83e617861af113b6412
Vishaka Vinod <vs235403@dal.ca>
2024-06-06 20:30:55 -0300

    added backend template

---
+++ flowers-almanac-backend/src/main/java/com/example/flowers_almanac_backend/FlowersAlmanacBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowers_almanac_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowersAlmanacBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowersAlmanacBackendApplication.class, args);
+	}
+
+}
---
+++ flowers-almanac-backend/src/test/java/com/example/flowers_almanac_backend/FlowersAlmanacBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowers_almanac_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowersAlmanacBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
