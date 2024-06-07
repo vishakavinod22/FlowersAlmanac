76c102797e885ea0eafdb5907cc169ba83edae87
Vishaka Vinod <vs235403@dal.ca>
2024-06-07 16:51:26 -0300

    minor change to download csv method

--- flowers-almanac/src/components/DownloadCsv.jsx
+++ flowers-almanac/src/components/DownloadCsv.jsx
@@ -4,20 +4,36 @@ import { Button} from 'react-bootstrap';
 
 function DownloadCsv(props){
     const [isDownload, setIsDownload] = useState(false);
+    const [message, setMessage] = useState('');
 
     const handleDownload = () => {
-        const url = props.url;
-        const link = document.createElement('a');
-        link.href = url;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        setIsDownload(true);
+        // const url = props.url;
+        // const link = document.createElement('a');
+        // link.href = url;
+        // document.body.appendChild(link);
+        // link.click();
+        // document.body.removeChild(link);
+        // setIsDownload(true);
+        try {
+            const url = props.url;
+    
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'flowers.csv');
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            setIsDownload(true);
+            setMessage('File has been downloaded!');
+        } catch (error) {
+            console.error('Error downloading file:', error);
+            setMessage('Error downloading file.');
+        }
     }
 
     return(
         <div className='flower-info-container'>
-            {!isDownload ? <p>Click button to download!</p> : <p>File has been downloaded!</p>}
+            {!isDownload ? <p>Click button to download!</p> : <p>{message}</p>}
             <Button className='btn' variant="dark" onClick={handleDownload}>Download</Button>
         </div>
     );
